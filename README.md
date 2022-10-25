9ece2bf1bfda47a30468cdae0a0dafb333257763
Ahmad Musa <ahmad@cloudify.co>
2022-10-26 00:50:13 +0300

    Add Build Instructions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # NetworkScannerCLI
 Network Scanner Through CLI Interface
+
+# Bulding the application 
+
+You go inside the src folder and run these set of commands
+
+```shell
+javac ScannerCLI.java
+jar -cvmf ScannerCLI.mf ScannerCLI.jar *.class
+```
+
+after that you would have ScannerCLI.jar, which you can invoke via 
+
+```shell
+java -jar ScannerCLI.jar
+```
--- src/ScannerCLI.java
+++ src/ScannerCLI.java
@@ -56,27 +56,6 @@ public class ScannerCLI {
     private static final String ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES";
 
-    public void progressPercentage(int remain, int total) {
-        if (remain > total) {
-            throw new IllegalArgumentException();
-        }
-        int maxBareSize = 10; // 10unit for 100%
-        int remainProcent = ((100 * remain) / total) / maxBareSize;
-        char defaultChar = '-';
-        String icon = "*";
-        String bare = new String(new char[maxBareSize]).replace('\0', defaultChar) + "]";
-        StringBuilder bareDone = new StringBuilder();
-        bareDone.append("[");
-        for (int i = 0; i < remainProcent; i++) {
-            bareDone.append(icon);
-        }
-        String bareRemain = bare.substring(remainProcent, bare.length());
-        System.out.print("\r" + bareDone + bareRemain + " " + remainProcent * 10 + "%");
-        if (remain == total) {
-            System.out.print("\n");
-        }
-    }
-
     // This function will try to get local network IPs of this machine
     public ArrayList<String> getMyIps() {
         ArrayList<String> ips = new ArrayList<String>();
@@ -353,7 +332,7 @@ public class ScannerCLI {
             switch(choice){
                 case 'a':
                     System.out.print("Would you like to provide details "+
-                        "or let the application scan you local network with all ports [true/false]: ");
+                        "or let the application scan your local network with all ports [true/false]: ");
                     boolean askForInputs = sc.nextBoolean();
                     if(askForInputs){
                         Boolean specificPorts = false;
